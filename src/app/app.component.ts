ea0acbd0ab47bb0652a9389fb4a5441116c5cd3d
HitendraSinghRathore <hitendersingh2@gmail.com>
2021-11-07 17:22:45 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { IList, ListData } from './data/list-data';
 
+declare const bootstrap:any;
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularList';
+export class AppComponent implements OnInit {
+  listSub = new BehaviorSubject<IList[]>([]);
+  listsData = this.listSub.asObservable();
+  selectedItem:IList | null = null;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.listSub.next(ListData);
+  }
+  listSelect(item:IList) {
+    this.selectedItem = item;
+  }
+  searched(searchString:string) {
+    if(searchString) {
+      const list = ListData.filter((item) => item.name.toLowerCase().indexOf(searchString.toLowerCase()) >=0);
+      this.listSub.next(list);
+    }else {
+      this.listSub.next(ListData);
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NgbModule, NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { ListsComponent } from './lists/lists.component';
+import { DetailsComponent } from './details/details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListsComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
+    NgbModule,
+    NgbTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
